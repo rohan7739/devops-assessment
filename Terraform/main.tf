30d74e056a8c352f19bf52177de828cdc75c9f57
Rohan Surve <surverohan786@gmail.com>
2026-01-29 16:06:02 +0530

    Added pipeline and cicd file

---
+++ Terraform/main.tf
@@ -0,0 +1,56 @@
+resource "aws_instance" "assignment_instance" {
+  ami = "ami-019715e0d74f695be"
+  instance_type = "t2.micro"
+  key_name = "nodeapp_mum-key"
+
+  vpc_security_group_ids = [
+    aws_security_group.instance_sg.id
+  ]
+
+#   security_groups = aws_security_group.instance_sg
+
+  tags = {
+    Name = "Ec2-assignment-vm"
+  }
+
+}
+
+resource "aws_security_group" "instance_sg" {
+  name        = "instance-security-group"
+  description = "Allow SSH, HTTP, and HTTPS"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "instance-sg"
+  }
+}
